7aa2dc69af5a486280d43ace2903666eb3a03d55
Maurice Herzog <mauriceder4te@gmail.com>
2024-02-02 09:27:46 +0100

    Projektdateien hinzufügen.

---
+++ Game.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Drawing;
+
+namespace Severus2
+{
+    internal class Game
+    {
+        
+            public static int Width { get; } = 26;
+            public static int Height { get; } = 26;
+
+            ConsoleKeyInfo keyInfo;
+            ConsoleKey consoleKey;
+
+            Snake snake;
+            Fruit fruit;
+
+            public int Score { set; get; }
+            bool IsLost;
+            bool IsWin;
+
+            public Game()
+            {
+                Console.CursorVisible = false;
+                Console.Title = "Error 404, Brain not found";
+                snake = new Snake();
+                fruit = new Fruit();
+            }
+
+            void Restart()
+            {
+                Board.Write(Width, Height);
+                Menu();
+
+
+                keyInfo = new ConsoleKeyInfo();
+                consoleKey = new ConsoleKey();
+
+                IsLost = false;
+                IsWin = false;
+
+                snake.Restart();
+                fruit.Restart();
+                Board.Write(Width, Height);
+            }
+
+            void Control()
+            {
+                if (Console.KeyAvailable)
+                {
+                    keyInfo = Console.ReadKey(true);
+                    consoleKey = keyInfo.Key;
+                }
+
+                switch (consoleKey)
+                {
+                    case ConsoleKey.W:
+                        if ((snake.Y[0] - snake.Y[1]) != 1) snake.Shift(Snake.Direction.Top);
+                        break;
+                    case ConsoleKey.S:
+                        if ((snake.Y[0] - snake.Y[1]) != -1) snake.Shift(Snake.Direction.Bottom);
+                        break;
+                    case ConsoleKey.A:
+                        if ((snake.X[0] - snake.X[1]) != 1) snake.Shift(Snake.Direction.Left);
+                        break;
+                    case ConsoleKey.D:
+                        if ((snake.X[0] - snake.X[1]) != -1) snake.Shift(Snake.Direction.Right);
+                        break;
+                    default:
+                        if ((snake.Y[0] - snake.Y[1]) == 1) snake.Shift(Snake.Direction.Bottom);
+                        else if ((snake.Y[0] - snake.Y[1]) == -1) snake.Shift(Snake.Direction.Top);
+                        else if ((snake.X[0] - snake.X[1]) == 1) snake.Shift(Snake.Direction.Right);
+                        else if ((snake.X[0] - snake.X[1]) == -1) snake.Shift(Snake.Direction.Left);
+                        break;
+                }
+            }
+
+
+            void Logic()
+            {
+
+                Control();
+                fruit.Logic(ref snake);
+                snake.Logic(ref IsLost, ref IsWin);
+            }
+
+            void Menu()
+            {
+                ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
+                ConsoleKey consoleKey = new ConsoleKey();
+                Console.SetCursorPosition(Width / 2 - 12, 1);
+                Console.Write("Error 404, Brain not found");
+                Console.SetCursorPosition(Width / 2 - 11, 2);
+                Console.Write("Press 1 to start new game");
+                Console.SetCursorPosition(Width / 2 - 11, 3);
+                Console.Write("Press Esc to quit a game");
+
+                while (true)
+                {
+                    keyInfo = Console.ReadKey(true);
+                    consoleKey = keyInfo.Key;
+                    switch (consoleKey)
+                    {
+                        case ConsoleKey.D1:
+                            return;
+                        case ConsoleKey.Escape:
+                            Environment.Exit(0);
+                            break;
+                    }
+                }
+            }
+
+
+            public void Play()
+            {
+                while (true)
+                {
+                    Restart();
+                    while (IsLost == false && IsWin == false)
+                    {
+                        Logic();
+                        Thread.Sleep(100);
+                        Console.Clear(); //################################
+                        Board.Write(Width, Height);
+                    }
+                    if (IsWin == true)
+                    {
+                        Console.SetCursorPosition(Height / 2 - 4, Width / 2);
+                        Console.Write("You win!!!");
+                    }
+                    else if (IsLost == true)
+                    {
+                        Console.SetCursorPosition(Height / 2 - 9, Width / 2);
+                        Console.Write("Try better next time");
+                    }
+                }
+            }
+        }
+    }
+
---
+++ Snake.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Severus2
+{
+    internal class Snake
+    {
+            public int[] X { private set; get; }
+            public int[] Y { private set; get; }
+            public int Length { set; get; }
+            public enum Direction { Left, Right, Top, Bottom }
+            public void Restart()
+            {
+                X = new int[50];
+                Y = new int[50];
+
+                X[0] = X[1] = X[2] = Game.Width / 2;
+                Y[0] = Game.Height / 2;
+                Y[1] = Game.Height / 2 + 1;
+                Y[2] = Game.Height / 2 + 2;
+
+                Length = 3;
+            }
+            public void Shift(Direction direction)
+            {
+                for (int i = Length + 1; i > 1; i--)
+                {
+                    X[i - 1] = X[i - 2];
+                    Y[i - 1] = Y[i - 2];
+                }
+
+                switch (direction)
+                {
+                    case Direction.Left:
+                        X[0]--;
+                        break;
+                    case Direction.Right:
+                        X[0]++;
+                        break;
+                    case Direction.Top:
+                        Y[0]--;
+                        break;
+                    case Direction.Bottom:
+                        Y[0]++;
+                        break;
+                }
+            }
+            void Draw()
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                for (int i = 0; i < Length; i++)
+                {
+
+                    Console.SetCursorPosition(X[i], Y[i]);
+                    Console.Write("■");
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+                if (X[Length] != 0)
+                {
+                    Console.SetCursorPosition(X[Length], Y[Length]);
+                    Console.Write("\0");
+                }
+            }
+            public void Logic(ref bool IsLost, ref bool IsWin)
+            {
+                if (Length == 50)
+                {
+                    IsWin = true;
+                }
+                else
+                {
+                    //Console.Clear();
+                    Draw();
+
+                    if (X[0] <= 0 || X[0] >= Game.Width + 1 || Y[0] <= 0 || Y[0] >= Game.Height + 1)
+                    {
+                        IsLost = true;
+                    }
+
+                    for (int i = Length; i >= 2; i--)
+                    {
+                        if (X[0] == X[i - 1] && Y[0] == Y[i - 1])
+                        {
+                            IsLost = true;
+                        }
+                    }
+                }
+            }
+
+
+            List<int> prevX = new List<int>();
+            List<int> prevY = new List<int>();
+
+
+            void DrawClean()
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                for (int i = 0; i < prevX.Count; i++)
+                {
+                    Console.SetCursorPosition(prevX[i], prevY[i]);
+                    Console.Write(" ");
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                for (int i = 0; i < Length; i++)
+                {
+                    Console.Clear();
+                    Console.SetCursorPosition(X[i], Y[i]);
+                    Console.Write("■");
+
+                }
+
+                prevX.Clear();
+                prevY.Clear();
+                for (int i = 0; i < Length; i++)
+                {
+                    prevX.Add(X[i]);
+                    prevY.Add(Y[i]);
+                }
+            }
+
+        }
+    }
+}
